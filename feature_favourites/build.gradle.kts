6e0de49fa304a3a6014313f5d4e960041cc2cc7e
Yamin Siahmargooei <yamin8000@yahoo.com>
2025-09-08 07:40:39 +0330

    fixed copyright

--- feature_favourites/build.gradle.kts
+++ feature_favourites/build.gradle.kts
@@ -1,3 +1,24 @@
+/*
+ *     freeDictionaryApp/freeDictionaryApp.feature_favourites
+ *     build.gradle.kts Copyrighted by Yamin Siahmargooei at 2025/9/8
+ *     build.gradle.kts Last modified at 2025/8/31
+ *     This file is part of freeDictionaryApp/freeDictionaryApp.feature_favourites.
+ *     Copyright (C) 2025  Yamin Siahmargooei
+ *
+ *     freeDictionaryApp/freeDictionaryApp.feature_favourites is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     freeDictionaryApp/freeDictionaryApp.feature_favourites is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with freeDictionaryApp.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
--- search/src/main/java/io/github/yamin8000/owl/search/ui/components/texts/WordTypeText.kt
+++ search/src/main/java/io/github/yamin8000/owl/search/ui/components/texts/WordTypeText.kt
@@ -1,3 +1,24 @@
+/*
+ *     freeDictionaryApp/freeDictionaryApp.search.main
+ *     WordTypeText.kt Copyrighted by Yamin Siahmargooei at 2025/9/8
+ *     WordTypeText.kt Last modified at 2025/8/31
+ *     This file is part of freeDictionaryApp/freeDictionaryApp.search.main.
+ *     Copyright (C) 2025  Yamin Siahmargooei
+ *
+ *     freeDictionaryApp/freeDictionaryApp.search.main is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     freeDictionaryApp/freeDictionaryApp.search.main is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with freeDictionaryApp.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.github.yamin8000.owl.search.ui.components.texts
 
 import androidx.compose.material.icons.Icons
