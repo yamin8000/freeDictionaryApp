d6ba045c22c00644a41e4460c62511611d5f94a7
Yamin Siahmargooei <yamin8000@yahoo.com>
2025-11-15 03:53:58 +0330

    added animation for ClickableIcon.kt

--- common/src/main/java/io/github/yamin8000/owl/common/ui/components/ClickableIcon.kt
+++ common/src/main/java/io/github/yamin8000/owl/common/ui/components/ClickableIcon.kt
@@ -21,26 +21,51 @@
 
 package io.github.yamin8000.owl.common.ui.components
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.twotone.Settings
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.yamin8000.owl.common.ui.theme.PreviewTheme
+
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    PreviewTheme {
+        ClickableIcon(
+            imageVector = Icons.TwoTone.Settings,
+            onClick = {}
+        )
+    }
+}
 
 @Composable
 fun ClickableIcon(
     imageVector: ImageVector,
-    contentDescription: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
-    enabled: Boolean = true
+    enabled: Boolean = true,
+    contentDescription: String? = null
 ) {
     val haptic = LocalHapticFeedback.current
+    val interactionSource = remember { MutableInteractionSource() }
+    val pressed by interactionSource.collectIsPressedAsState()
+    val rotation by animateFloatAsState(targetValue = if (pressed) 15f else 0f)
     IconButton(
-        modifier = modifier,
+        modifier = modifier.graphicsLayer { rotationZ = rotation },
         enabled = enabled,
+        interactionSource = interactionSource,
         onClick = {
             haptic.performHapticFeedback(HapticFeedbackType.ContextClick)
             onClick()
