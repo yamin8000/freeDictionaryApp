84c4c51e30259006a00a458f120c138b35a1872a
Yamin Siahmargooei <yamin8000@yahoo.com>
2026-06-17 04:31:04 +0330

    minor reformat

--- feature_about/src/main/kotlin/io/github/yamin8000/owl/feature_about/ui/AboutViewModel.kt
+++ feature_about/src/main/kotlin/io/github/yamin8000/owl/feature_about/ui/AboutViewModel.kt
@@ -36,7 +36,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
